ce3cf0c2fa3a9d697ee116dbfeda4b449410eddb
emil <emil@EmilsLaptop>
2017-12-13 11:23:16 +0100

    Traversing in x should work (part 3)

--- PencilPrinter/src/pencilprinter/PencilPrinter.java
+++ PencilPrinter/src/pencilprinter/PencilPrinter.java
@@ -116,11 +116,11 @@ public class PencilPrinter {
         //byte[] via byteConvert and send it with tcpClient.write()
         for (int i = 0; i < boolArray.length; i++) {
         boolArrayRow = boolArray[i];
-        
+        /*
         //Printing the boolArray
-            for (int j = 0; j <= 255; j++) {
-                System.out.println("y: "+i+" - x: "+j+": "+boolArray[i][j]);
-            }
+        for (int j = 0; j <= 255; j++) {
+        System.out.println("y: "+i+" - x: "+j+": "+boolArray[i][j]);
+        }*/
         
         //Converting the boolArray
         tmpByte = byteConvert.boolToByte(boolArrayRow);
